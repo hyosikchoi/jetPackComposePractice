5c0d11275979082924af51ec30424cdcd33acb37
ChoiHyoSik <huchoi24@naver.com>
2022-04-27 17:12:16 +0900

    컴포즈 Column 예제

---
+++ app/src/main/java/kr/co/composeex/MainActivity.kt
@@ -0,0 +1,88 @@
+package kr.co.composeex
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kr.co.composeex.ui.theme.ComposeExTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeExTheme {
+                // A surface container using the 'background' color from the theme
+//                Column(modifier = Modifier.padding(24.dp)) {
+//                    Greeting(name = "안녕")
+//                    Greeting(name = "잘지내")
+//                    Greeting(name = "나야나")
+//                }
+//                Greeting(names = listOf<String>("안녕","잘가","잘지내"))
+                MyApp()
+            }
+        }
+
+//        lifecycleScope.launch {
+//            val stTime = System.currentTimeMillis()
+//            val lastName : Deferred<String> = async {
+//                delay(1000)
+//                "최"
+//            }
+//            val name : Deferred<String> = async {
+//                delay(2000)
+//                "효식"
+//            }
+//
+//            Log.d("lastname" , "${lastName.await()}${name.await()} , time : ${System.currentTimeMillis() - stTime}")
+//        }
+
+    }
+}
+
+@Composable
+fun MyApp(names: List<String> = listOf<String>("안녕","잘가","잘지내")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for(name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp , horizontal = 8.dp)
+    ) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+        }
+    }
+}
+
+@Preview(showBackground = true , widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    ComposeExTheme {
+        MyApp()
+    }
+}
