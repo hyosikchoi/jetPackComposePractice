358b5b8491eb347f8db5dabe7f00e459ae4a591f
ChoiHyoSik <huchoi24@naver.com>
2022-04-28 14:16:17 +0900

    theme 및 color 설정

--- app/src/main/java/kr/co/composeex/MainActivity.kt
+++ app/src/main/java/kr/co/composeex/MainActivity.kt
@@ -1,5 +1,6 @@
 package kr.co.composeex
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -15,6 +16,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
@@ -130,7 +132,9 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name , style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
             OutlinedButton(onClick = { expanded.value = !expanded.value }) {
                 Text(if(expanded.value) "Show less" else "Show more")
@@ -138,7 +142,12 @@ fun Greeting(name: String) {
         }
     }
 }
-
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
