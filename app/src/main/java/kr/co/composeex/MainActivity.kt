e80c55c6fc55c148181fc3e3b68bdc19e9c5baf7
ChoiHyoSik <huchoi24@naver.com>
2022-04-28 15:28:08 +0900

    Card로 교체 및 IconButton으로 expanded 교체

--- app/src/main/java/kr/co/composeex/MainActivity.kt
+++ app/src/main/java/kr/co/composeex/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -12,10 +13,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,6 +30,7 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kr.co.composeex.ui.theme.ComposeExTheme
+import kr.co.composeex.R
 
 
 class MainActivity : ComponentActivity() {
@@ -108,39 +114,61 @@ fun MyApp(names: List<String> = List(1000) {"$it"}) {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Greeting(name : String) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp , horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+fun CardContent(name: String) {
     /** remember를 사용하여 state가 다시 false로 리컴포지션 되는것을 방지 */
     /** rememberSaveable로 변경하여 상태값을 저장하여 화면회전이나 스크롤이동시에도 변함없이 되게끔 적용 */
     val expanded = rememberSaveable { mutableStateOf(false)}
 
-    val extraPadding by animateDpAsState(
-        if(expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
 
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
+        ) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                    .padding(12.dp)
             ) {
                 Text(text = "Hello, ")
                 Text(text = name , style = MaterialTheme.typography.h4.copy(
                     fontWeight = FontWeight.ExtraBold
                 ))
+                if(expanded.value) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4)
+                    )
+                }
             }
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if(expanded.value) "Show less" else "Show more")
+            IconButton(onClick = { expanded.value = !expanded.value }) {
+                Icon(
+                    imageVector = if (expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded.value) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+
+                )
             }
         }
-    }
+
 }
 @Preview(
     showBackground = true,
