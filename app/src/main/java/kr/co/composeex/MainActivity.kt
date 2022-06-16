9430b3e4395c79bb6423dd63b52ee33ca80ca0ad
hyosikchoi <huchoi24@naver.com>
2022-06-17 02:06:28 +0900

    compose UI조정 연습

--- app/src/main/java/kr/co/composeex/MainActivity.kt
+++ app/src/main/java/kr/co/composeex/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -24,6 +25,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
@@ -64,7 +66,9 @@ fun OnboardingScreen(onContinueClicked : () -> Unit) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Welcome to the Basics Codelab!")
+            Text(text = "Welcome to the Basics Codelab!",
+                 fontSize = 20.sp
+                )
             Button(
                 modifier = Modifier.padding(vertical = 24.dp),
                 onClick = onContinueClicked
@@ -112,7 +116,7 @@ fun CardContent(name: String) {
                 .padding(12.dp)
                 .animateContentSize(
                     animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        dampingRatio = Spring.DampingRatioHighBouncy,
                         stiffness = Spring.StiffnessLow
                     )
                 )
